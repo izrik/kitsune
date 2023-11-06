12e992ca2ac35561ebe2b7c70daaab33f6c55711
izrik <izrik@izrik.com>
2023-11-06 13:35:02 -0600

    Rename the popup file and add some inputs.

---
+++ popup/popup.js
@@ -0,0 +1,5 @@
+window.onload = async () => {
+    const userWindowTitleInput = document.querySelector('#user-window-title-input');
+    userWindowTitleInput.value = "This is where the title goes";
+    userWindowTitleInput.select();
+};
