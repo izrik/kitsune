eee02f1d8559533bbda40d91e47f0c263a2b9693
izrik <izrik@izrik.com>
2025-09-06 22:28:45 -0500

    Ignore more files.

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 VERSION="$(cat manifest.json | jq -r .version)"
-web-ext build -n kitsune-$VERSION.zip -o -i CLAUDE.md kitsune-windows*.json build.sh
+web-ext build -n kitsune-$VERSION.zip -o -i CLAUDE.md kitsune-windows*.json *.sh *.py instance notes.md
