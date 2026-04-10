dbc79cf0ae708b950fb6398b9fef48fb774d07d6
izrik <izrik@izrik.com>
2026-04-10 18:15:02 -0500

    Complete the signing script.

--- sign.sh
+++ sign.sh
@@ -3,4 +3,5 @@ web-ext sign \
   -i CLAUDE.md kitsune-windows*.json *.sh *.py instance notes.md \
   -i tools logo package.json package-lock.json \
   -i icons/bolt.png icons/read_more.png icons/sunny.png icons/kitsune.svg \
-  --api-key
+  --api-key "$AMO_JWT_ISSUER" \
+  --api-secret "$AMO_JWT_SECRET"
