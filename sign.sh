7d99b751e987fd69b3d40510403dadbe6a22daa1
izrik <izrik@izrik.com>
2026-04-11 09:08:50 -0500

    Don't use npx

--- sign.sh
+++ sign.sh
@@ -1,5 +1,5 @@
 VERSION="$(cat manifest.json | jq -r .version)"
-npx web-ext sign \
+web-ext sign \
   -i CLAUDE.md kitsune-windows*.json *.sh *.py instance notes.md \
   -i tools logo package.json package-lock.json \
   -i icons/bolt.png icons/read_more.png icons/sunny.png icons/kitsune.svg \
